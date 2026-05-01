cd1d5da73936f31709500b5b130f51771ac7b37f
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-01 14:20:49 +0600

    feat: add text in sign-up/sign-in page

--- src/app/(auth)/auth/signin/page.jsx
+++ src/app/(auth)/auth/signin/page.jsx
@@ -2,7 +2,7 @@ const SignInPage = () => {
 
     return (
         <div>
-
+            <h1>Sign-In Page</h1>
         </div>
     );
 };
--- src/app/(auth)/auth/signup/page.jsx
+++ src/app/(auth)/auth/signup/page.jsx
@@ -2,7 +2,7 @@ const SignUpPage = () => {
 
     return (
         <div>
-
+            <h1>Sign-Up Page</h1>
         </div>
     );
 };
