2cc3aadf658cd7559db991d545ffc41494289fda
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-01 17:00:55 +0600

    feat: create container component

---
+++ src/components/ui/Container/Container.jsx
@@ -0,0 +1,10 @@
+const Container = ({ children }) => {
+
+    return (
+        <div className="w-full max-w-[1600px] mx-auto">
+            { children }
+        </div>
+    );
+};
+
+export default Container;
