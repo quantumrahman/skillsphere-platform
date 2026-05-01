4bbf5cbbb8adbe13b3c9612410727a07baa84aa2
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-01 19:35:27 +0600

    feat: header nav design done

--- src/components/ui/Navbars/HeaderNav/HeaderNav.jsx
+++ src/components/ui/Navbars/HeaderNav/HeaderNav.jsx
@@ -1,8 +1,24 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import headerLinks from "@/constants/headerLInks";
+import Link from "next/link";
+
 const HeaderNav = () => {
+    const pathname = usePathname();
 
     return (
-        <div>
-
+        <div className="flex items-center">
+            {headerLinks.map((link) => (
+                <Link 
+                    key={link?.id} 
+                    href={link?.path}
+                    className={`inline-block text-sm font-normal text-[#262626] rounded-md transition-all duration-200 ease-in-out px-5 py-3 lg:px-6 lg:py-[14px] lg:text-lg ${pathname === link?.path ? 'bg-[#F1F1F3]' : 'bg-[#f7f7f8] hover:underline'}`}
+                >
+                    {link?.name}
+                </Link>
+            ))}
         </div>
     );
 };
