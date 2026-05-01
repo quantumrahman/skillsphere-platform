77457f5ceed4fdce454c4fa12267a84fef46a6c8
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-01 19:36:21 +0600

    feat: sign-in button design done

--- src/components/ui/Buttons/SignInButton/SignInButton.jsx
+++ src/components/ui/Buttons/SignInButton/SignInButton.jsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
+
 const SignInButton = () => {
 
     return (
-        <div>
-
-        </div>
+        <Link href={'/auth/signin'}>
+            <button className="px-6 py-3 bg-[#FF9500] rounded-md cursor-pointer transition-all duration-300 ease-linear lg:px-[34px] lg:rounded-[8px]">
+                <span className="text-sm font-normal text-white transition-all duration-300 ease-linear lg:text-lg">
+                    Login
+                </span>
+            </button>
+        </Link>
     );
 };
 
