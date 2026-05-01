be6d83d6350cb29ba6dd82ddec58d22860e7efd3
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-01 20:21:43 +0600

    feat: create footer info file

---
+++ src/constants/footerInfo.js
@@ -0,0 +1,93 @@
+import mail from "@/assets/icons/mail.png";
+import phone from "@/assets/icons/phone.png";
+import location from "@/assets/icons/location.png";
+import facebook from "@/assets/icons/facebook.png";
+import twitter from "@/assets/icons/twitter.png";
+import linkedin from "@/assets/icons/linkedin.png";
+
+export const contactInfo = [
+    {
+        id: 1,
+        icon: mail,
+        mail: 'skillsphere@learn.com',
+    },
+    {
+        id: 2,
+        icon: phone,
+        phone: '+880 1254-568954',
+    },
+    {
+        id: 3,
+        icon: location,
+        location: 'Dhaka, Bangladesh',
+    },
+];
+
+export const socialInfo = [
+    {
+        id: 1,
+        icon: facebook,
+        path: '/',
+    },
+    {
+        id: 2,
+        icon: twitter,
+        path: '/',
+    },
+    {
+        id: 3,
+        icon: linkedin,
+        path: '/',
+    },
+];
+
+export const legalInfo = [
+    {
+        id: 1,
+        path: '/',
+        name: 'Terms & Conditions',
+    },
+    {
+        id: 2,
+        path: '/',
+        name: 'Privacy Policy',
+    },
+    {
+        id: 3,
+        path: '/',
+        name: 'Refund Policy',
+    },
+    {
+        id: 4,
+        path: '/',
+        name: 'Cookie Policy',
+    },
+];
+
+export const quickLinks = [
+    {
+        id: 1,
+        path: '/',
+        name: 'Home',
+    },
+    {
+        id: 2,
+        path: '/courses',
+        name: 'Courses',
+    },
+    {
+        id: 3,
+        path: '/about',
+        name: 'About Us',
+    },
+    {
+        id: 4,
+        path: '/contact',
+        name: 'Contact',
+    },
+    {
+        id: 5,
+        path: '/blog',
+        name: 'Blog',
+    },
+];
