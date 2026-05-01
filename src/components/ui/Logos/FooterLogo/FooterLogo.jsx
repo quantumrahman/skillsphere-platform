a3d57f4ceac21389078f1eda25f7bbc7983fee26
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-01 22:02:27 +0600

    feat: add logo

--- src/components/ui/Logos/FooterLogo/FooterLogo.jsx
+++ src/components/ui/Logos/FooterLogo/FooterLogo.jsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+import logo from '@/assets/image/logo.png';
+
 const FooterLogo = () => {
 
     return (
-        <div>
-
+        <div className="w-[40px] h-[40px] bg-[#FF9500] rounded-[6px] px-2 py-4 flex items-center justify-center transition-all duration-300 ease-linear md:w-[44px] md:h-[44px] md:rounded-[8px] md:px-[5.5px] md:py-[5.5px] lg:w-[54px] lg:h-[54px] lg:px-[7px] lg:py-[7px]">
+            <div className="w-[30px] h-[30px] relative transition-all duration-300 ease-linear md:w-[32px] md:h-[32px] lg:w-[40px] lg:h-[40px]">
+                <Image 
+                    src={logo} 
+                    alt="SkillSphere logo" 
+                    fill={true} 
+                    className="w-full h-auto object-cover transition-all duration-300 ease-linear" 
+                />
+            </div>
         </div>
     );
 };
