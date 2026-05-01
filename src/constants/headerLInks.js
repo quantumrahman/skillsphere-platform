9685606ccd64c2c9cfd85505952400c47b30fb26
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-01 18:48:36 +0600

    feat: create headerLInks.js for header links

---
+++ src/constants/headerLInks.js
@@ -0,0 +1,19 @@
+const headerLinks = [
+    {
+        id: 1,
+        path: '/',
+        name: 'Home'
+    },
+    {
+        id: 2,
+        path: '/courses',
+        name: 'Courses'
+    },
+    {
+        id: 3,
+        path: '/profile',
+        name: 'Profile'
+    }
+];
+
+export default headerLinks;
