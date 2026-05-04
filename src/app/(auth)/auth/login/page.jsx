013af4d40fe47cf26140bfc02321db6a0b3a8d97
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-04 21:36:23 +0600

    feat: implement logo with text

--- src/app/(auth)/auth/login/page.jsx
+++ src/app/(auth)/auth/login/page.jsx
@@ -1,8 +1,15 @@
+import Logo from "@/components/ui/Logo/Logo.jsx";
+
 const LoginPage = () => {
 
     return (
         <div className="w-full min-h-screen flex items-center justify-center">
-            <h1>Login Page</h1>
+            <div className="w-full max-w-[660px] px-5">
+                <div className="flex items-center justify-center gap-3">
+                    <Logo />
+                    <h1 className="text-[28px] font-semibold text-[#E6FFF9] md:text-[32px] lg:text-[38px] transition-all duration-300 ease-linear">SkillSphere</h1>
+                </div>
+            </div>
         </div>
     );
 };
