5d037bccfb8cdae4dcdc79c4778737e40e8f1683
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-05 11:52:40 +0600

    feat: create links.js and add navlinks array

---
+++ src/constants/links.js
@@ -0,0 +1,17 @@
+export const navlinks = [
+    {
+        id: 1,
+        path: '/',
+        name: "Home"
+    },
+    {
+        id: 2,
+        path: '/courses',
+        name: "Courses"
+    },
+    {
+        id: 3,
+        path: '/profile',
+        name: "Profile"
+    },
+];
