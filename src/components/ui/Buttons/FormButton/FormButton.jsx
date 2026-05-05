3a30c7cbab6686617a6b56e1370aaa102063f1a4
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-05 07:39:57 +0600

    feat: implement reusable form button design

--- src/components/ui/Buttons/FormButton/FormButton.jsx
+++ src/components/ui/Buttons/FormButton/FormButton.jsx
@@ -1,8 +1,14 @@
-const FormButton = () => {
+const FormButton = ({ children, onClick }) => {
 
     return (
-        <div>
-            
+        <div className="w-full">
+            <button
+                type="submit"
+                onClick={onClick}
+                className="w-full py-[14px] bg-gradient-to-r from-[#06231D] via-[#0C342C] to-[#076653] rounded-full lg:py-[18px] text-sm font-medium text-[#E6FFF9] md:text-lg cursor-pointer transition-all duration-300 ease-linear"
+            >
+                {children}
+            </button>
         </div>
     );
 };
