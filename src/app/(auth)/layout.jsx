1036d4e5851310713e1c68d128330ceb069a2d41
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-05 07:52:18 +0600

    feat: add copyright text at the bottom

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -14,8 +14,11 @@ export const metadata = {
 
 export default function AuthLayout({ children }) {
     return (
-        <div className={`${beVietnamePro.variable} w-full min-h-screen antialiased bg-black bg-[radial-gradient(circle_at_top,#0C342C_0%,#050A08_60%,#000000_100%)]`}>
+        <div className={`${beVietnamePro.variable} w-full min-h-screen antialiased bg-black bg-[radial-gradient(circle_at_top,#0C342C_0%,#050A08_60%,#000000_100%)] relative`}>
             {children}
+            <div className="w-full text-center py-4 absolute bottom-0 px-5">
+                <span className="text-sm text-[#5C8F86]">Copyright &copy; 2026. Developed by Rakibul Rahman</span>
+            </div>
         </div>
     );
 };
