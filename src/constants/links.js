0a95f139d3ac79b2369285482dbeb02528e54a72
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-05 13:48:20 +0600

    feat: add some footer link

--- src/constants/links.js
+++ src/constants/links.js
@@ -15,3 +15,70 @@ export const navlinks = [
         name: "Profile"
     },
 ];
+
+export const socialLinks = [
+    {
+        id: 1,
+        path: '/',
+        icon: 'facebook'
+    },
+    {
+        id: 2,
+        path: '/',
+        icon: 'github'
+    },
+    {
+        id: 3,
+        path: '/',
+        icon: 'linkedin'
+    },
+];
+
+export const quickLinks = [
+    {
+        id: 1,
+        path: '/',
+        name: 'Home'
+    },
+    {
+        id: 2,
+        path: '/courses',
+        name: 'Courses'
+    },
+    {
+        id: 3,
+        path: '/profile',
+        name: 'Profile'
+    },
+];
+
+export const legalLinks = [
+    {
+        id: 1,
+        path: '/',
+        name: 'Term & Conditions',
+    },
+    {
+        id: 2,
+        path: '/',
+        name: 'Privacy Policy',
+    },
+];
+
+export const contactLinks = [
+    {
+        id: 1,
+        icon: 'mail',
+        email: 'support@skillsphere.com'
+    },
+    {
+        id: 2,
+        icon: 'phone',
+        phone: '+880 1200-558822'
+    },
+    {
+        id: 3,
+        icon: 'location',
+        location: 'Barisal, Bangladesh'
+    },
+];
